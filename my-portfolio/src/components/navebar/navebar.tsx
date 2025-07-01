a39a3453eec56b16640debb9b65bf09214a274ff
Mouad Serroukh <mserrouk@e2r5p7.1337.ma>
2025-07-01 12:06:23 +0100

    feat(navebar): update image size

--- my-portfolio/src/components/navebar/navebar.tsx
+++ my-portfolio/src/components/navebar/navebar.tsx
@@ -65,8 +65,8 @@ const Navebar = () => {
                     <div>
                         <Image
                             src="/final.png"
-                            width={70}
-                            height={70}
+                            width={50}
+                            height={50}
                             alt="Picture of the author"
                             />
                     </div>
